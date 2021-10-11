dc938e1348c2c695f58ee2eff75574e3d540f0e7
Oran <oranwald@gmail.com>
2021-10-11 10:24:00 +0100

    Added Pawn movement

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from "../square.js"
+import Player from "../player.js"
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,23 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
+        let currentSquare = board.findPiece(this);
+        if (this.player === Player.WHITE){
+            if (currentSquare.row === 1){
+                return [Square.at(currentSquare.row + 2, currentSquare.col), Square.at(currentSquare.row + 1, currentSquare.col)];
+            }
+            else{
+                return [Square.at(currentSquare.row + 1, currentSquare.col)];
+            }
+        }
+        if (this.player === Player.BLACK){
+            if (currentSquare.row === 6){
+                return [Square.at(currentSquare.row - 2, currentSquare.col), Square.at(currentSquare.row - 1, currentSquare.col)];
+            }
+            else{
+                return [Square.at(currentSquare.row - 1, currentSquare.col)];
+            }
+        }
         return new Array(0);
     }
 }
